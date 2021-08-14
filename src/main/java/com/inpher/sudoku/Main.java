55ddcf27a95737cf6c0f1cc97c41c39df0734d1c
Agustin Cabra <cabra.agustin@gmail.com>
2021-08-14 08:57:04 +0200

    basic bootstrap application that bundles the jar file and logs the entry

---
+++ src/main/java/com/inpher/sudoku/Main.java
@@ -0,0 +1,12 @@
+package com.inpher.sudoku;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String... args) {
+        logger.info("running...");
+    }
+}
